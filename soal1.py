b9fdd9b6053ac4307df92bff0a806790c4b53d1b
Rio-arbanianto <117901421+Rio-arbanianto@users.noreply.github.com>
2023-01-03 14:55:08 +0700

    Add files via upload

---
+++ soal1.py
@@ -0,0 +1,80 @@
+# print("============================================")
+# print("************Justice language****************")
+# nama=input("Masukan user name anda :")
+# print("WELCOME victor")
+
+
+
+
+print("===selamat datang di XXV===")
+a=input("masukan tanggal hari ini :")
+if a ==12:
+ def film():
+    print("Berikut genre film yng tersedia")
+    print("1. horor")
+    print("2. action")
+    pil=(input("silahkan pilih mau nonton film bergenre apa :"))
+    if pil<=2:
+        genre()
+    else:
+        print("pilihan yang anda pilih tidak tersedia di bioskop ini")
+ 
+ def genre():
+    print("==berikut pilihan film horor yang sedang tayang==")
+    print("1. the devil's light")
+    print("2. pengabdi setan")
+
+    a=int(input("silahkan mau nonton film bergenre apa :"))
+    b=int(input("silahkan mau nonton film bergenre apa :"))
+    total=b*24500
+    print ("total yang harus dibayar adalah rp.",total)
+
+elif a == 5:
+ def film():
+    print("Berikut genre film yng tersedia")
+    print("1. horor")
+    print("2. action")
+
+    pil=int(input("silahkan pilih mau nonton film bergenre apa :"))
+    if pil<=2:
+        genre()
+    else:
+        print("pilihan yang anda pilih tidak tersedia di bioskop ini")
+        
+ def genre():
+    print("==berikut pilihan film horor yang sedang tayang==")
+    print("1. black panther : Wakanda vorever")
+    print("2. avatar : the way of water")
+
+    a=int(input("silahkan mau nonton film bergenre apa :"))
+    b=int(input("silahkan mau nonton film bergenre apa :"))
+    total=b*24500
+    print ("total yang harus dibayar adalah rp.",total)
+
+elif a == 18:
+ def film():
+    print("Berikut genre film yng tersedia")
+    print("1. horor")
+    print("2. action")
+
+    pil=int(input("silahkan pilih mau nonton film bergenre apa :"))
+    if pil<=3:
+        genre()
+    else:
+        print("pilihan yang anda pilih tidak tersedia di bioskop ini")
+        
+ def genre():
+    print("==berikut pilihan film horor yang sedang tayang==")
+    print("1. the devil's light")
+    print("2. pengabdi setan")
+
+    a=int(input("silahkan mau nonton film bergenre apa :"))
+    b=int(input("silahkan mau nonton film bergenre apa :"))
+    total=b*24500
+    print ("total yang harus dibayar adalah rp.",total)
+
+ film()
+
+
+
+
